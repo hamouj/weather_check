e3b3861faa43aef4b2dca61cfa314dd3eedbbe77
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-22 08:02:42 -0700

    feat: create MapquestService and get_lat_long() method

---
+++ app/services/mapquest_service.rb
@@ -0,0 +1,16 @@
+class MapquestService
+  def self.get_lat_long(location)
+    get_url("address?location=#{location}")
+  end
+  
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  
+  def self.conn
+    Faraday.new(url: 'https://www.mapquestapi.com/geocoding/v1/') do |faraday|
+      faraday.params['key'] = ENV['mapquest_api_key']
+    end
+  end
+end
