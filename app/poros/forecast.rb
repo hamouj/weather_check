f70d4d94851984ccd5b9f3d36a00e24b2f5633cf
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-22 14:49:33 -0700

    fix: update poro name and add nil id

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -2,12 +2,14 @@ require './app/poros/current_weather.rb'
 require './app/poros/daily_weather.rb'
 require './app/poros/hourly_weather.rb'
 
-class Weather
-  attr_reader :current_weather,
+class Forecast
+  attr_reader :id,
+              :current_weather,
               :daily_weather,
               :hourly_weather
 
   def initialize(data)
+    @id = nil
     @current_weather = CurrentWeather.new(data[:current])
     @daily_weather = data[:forecast][:forecastday].map { |day_data| DailyWeather.new(day_data) }
     @hourly_weather = data[:forecast][:forecastday][0][:hour].map { |hour_data| HourlyWeather.new(hour_data) }
