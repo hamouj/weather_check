ac5844c2bea1e68a2a77de8b31c31033834a1f88
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-22 09:55:56 -0700

    feat: create weather service with #get_forecast() and #get_hourly_weather() class methods

---
+++ app/services/weather_service.rb
@@ -0,0 +1,20 @@
+class WeatherService
+  def self.get_forecast(lat_lng)
+    get_url("v1/forecast.json?q=#{lat_lng}&days=5")
+  end
+
+  def self.get_hourly_weather(lat_lng)
+    get_url("v1/forecast.json?q=#{lat_lng}&hour=0-24")
+  end
+
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  
+  def self.conn
+    Faraday.new(url: 'http://api.weatherapi.com/') do |faraday|
+      faraday.params[:key] = ENV['weather_api_key']
+    end
+  end
+end
