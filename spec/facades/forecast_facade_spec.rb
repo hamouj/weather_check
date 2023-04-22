4e11f078dc2c8c798cf2953bd92768f5152469a5
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-22 15:13:43 -0700

    test: remove mapquest facade test and combine with forecast facade spec

---
+++ spec/facades/forecast_facade_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe ForecastFacade do
+  describe 'instance methods' do
+    before (:each) do
+      @facade = ForecastFacade.new('lasvegas,nv')
+    end
+
+    context '#initialize' do
+      it 'exists' do
+        expect(@facade).to be_a(ForecastFacade)
+      end
+    end
+
+    context '#find_lat_lng' do
+      it 'creates a Location object' do
+        VCR.use_cassette('lat_lng_lv', serialize_with: :json, match_requests_on: [:method, :path]) do
+          expect(@facade.find_lat_lng).to be_a(Location)
+        end
+      end
+    end
+
+    context '#fetch_forecast' do
+      it 'creates a Forecast object' do
+        VCR.use_cassette('weather_forecast_lv', serialize_with: :json, match_requests_on: [:method, :path]) do
+          expect(@facade.fetch_forecast).to be_a(Forecast)
+        end
+      end
+    end
+  end
+end
