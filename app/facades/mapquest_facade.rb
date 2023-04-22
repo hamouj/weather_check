574f72ee1bc465d4d9f82ac009ddaf1dd20dfacd
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-22 09:06:16 -0700

    feat: create mapquest facade with find_lat_lng instance method

---
+++ app/facades/mapquest_facade.rb
@@ -0,0 +1,12 @@
+class MapquestFacade
+  attr_reader :location
+
+  def initialize(location)
+    @location = location
+  end
+
+  def find_lat_lng
+    data = MapquestService.get_lat_lng(@location)[:results][0][:locations][0][:latLng]
+    Location.new(data)
+  end
+end
