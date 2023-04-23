d3fc783d8d9a085f87a1a13f9bd79f61909617ef
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-23 10:34:09 -0700

    fix: update fetch_forecast method and tests

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -5,19 +5,9 @@ class Api::V0::ForecastController < ApplicationController
   def index
     if params[:location].present?
       forecast = ForecastFacade.new(params[:location]).fetch_forecast
-      location_check(forecast)
+      render json: ForecastSerializer.new(forecast)
     else
       render json: ErrorSerializer.new('A location must be provided').invalid_request, status: 404
-    end
-  end
-
-  private
-
-  def location_check(forecast)
-    if forecast == "Location not found"
-      render json: ErrorSerializer.new(forecast).invalid_request, status: 404
-    else
-      render json: ForecastSerializer.new(forecast).serializable_hash.to_json
     end
   end
 end
--- spec/requests/api/v0/forecast_request_spec.rb
+++ spec/requests/api/v0/forecast_request_spec.rb
@@ -136,15 +136,13 @@ describe 'Forecast API' do
   end
 
   describe 'edge case testing' do
-    it 'returns an error object when the location entered does not exist' do
+    it 'returns a fallback location when the location entered does not exist' do
       VCR.use_cassette('incorrect_location', serialize_with: :json) do
         get "/api/v0/forecast?location=akjsdf,mnp"
 
         response_body = JSON.parse(response.body, symbolize_names: true)
-        expect(response_body).to have_key(:errors)
-        expect(response_body[:errors][0][:status]).to eq('404')
-        expect(response_body[:errors][0][:title]).to eq('Invalid Request')
-        expect(response_body[:errors][0][:detail].first).to eq('Location not found')
+        expect(response_body).to have_key :data
+        expect(response_body[:data].keys).to eq([:id, :type, :attributes])
       end
     end
   end
