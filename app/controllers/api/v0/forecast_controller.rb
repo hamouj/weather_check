621126621c1d6e0d39b2b51a754130fd2fa97c87
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-22 21:04:04 -0700

    feat: add error handling to forecast endpoint

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -3,7 +3,21 @@
 # app/controllers/api/v0/forecast_controller.rb
 class Api::V0::ForecastController < ApplicationController
   def index
-    forecast = ForecastFacade.new(params[:location]).fetch_forecast
-    render json: ForecastSerializer.new(forecast).serializable_hash.to_json
+    if params[:location].present?
+      forecast = ForecastFacade.new(params[:location]).fetch_forecast
+      location_check(forecast)
+    else
+      render json: ErrorSerializer.new('A location must be provided').invalid_request, status: 404
+    end
   end
+
+  private
+
+    def location_check(forecast)
+      if forecast == "Location not found"
+        render json: ErrorSerializer.new(forecast).invalid_request, status: 404
+      else
+        render json: ForecastSerializer.new(forecast).serializable_hash.to_json
+      end
+    end
 end
