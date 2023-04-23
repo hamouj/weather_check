73e34d7793d6acb28ee82d255b6f1262a8b23690
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-23 14:35:13 -0700

    feat: create user model with validations and private method used in callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,16 @@
 
 # app/models/user.rb
 class User < ApplicationRecord
-  validates :email, :password, presence: true
-  validates :email, uniqueness: true
+  before_save :generate_api_key
 
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
+  
   has_secure_password
+
+  private
+    def generate_api_key
+      self.api_key = SecureRandom.hex
+    end
 end
