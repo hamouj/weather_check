da59e760f5576389d4f6407a28f959f95e58af63
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-24 10:06:49 -0700

    feat: create salaries serializer to format response

--- app/serializers/salaries_serializer.rb
+++ app/serializers/salaries_serializer.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 # app/serializers/salary_serializer.rb
-class SalarySerializer
+class SalariesSerializer
   def initialize(destination)
     @destination = destination
     @forecast = ForecastFacade.new(@destination).fetch_forecast
     @salaries = SalaryFacade.new(@destination).fetch_salaries
   end
 
-  def self.send_salaries
+  def serialize_salaries
     {
       "data": {
-        "id": null,
+        "id": nil,
         "type": "salaries",
         "attributes": {
           "destination": @destination,
@@ -19,13 +19,14 @@ class SalarySerializer
             "summary": @forecast.current_weather.condition,
             "temperature": @forecast.current_weather.temperature
           },
-          "salaries": [
+          "salaries":
             @salaries.each do |salary|
+              {
               "title": salary.title,
               "min": salary.min,
               "max": salary.max
+              }
             end
-          ]
         }
       }
     }
