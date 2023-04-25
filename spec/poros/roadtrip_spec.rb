5f7757adc441c32a0a473b157bda342a1a71008b
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-24 18:38:10 -0700

    fix: update test to reflect updated time and travel time attributes

--- spec/poros/roadtrip_spec.rb
+++ spec/poros/roadtrip_spec.rb
@@ -14,12 +14,29 @@ describe Roadtrip do
           }
         }
 
-        lv_to_denver = Roadtrip.new({origin: 'las vegas,nv', destination: 'denver,co'}, roadtrip_data)
+        lv_to_denver = Roadtrip.new('las vegas,nv', 'denver,co', roadtrip_data)
 
         expect(lv_to_denver).to be_a Roadtrip
         expect(lv_to_denver.start_city).to eq('las vegas,nv')
         expect(lv_to_denver.end_city).to eq('denver,co')
         expect(lv_to_denver.travel_time).to eq('10h32m')
+        expect(lv_to_denver.time).to eq('10:32:56')
+      end
+    end
+
+    context '#format_travel_time()' do
+      it 'formats travel time into days, hours, minutes' do
+        roadtrip_data = {
+          "route": {
+            "distance": 747.6798,
+            "time": 289919,
+            "formattedTime": "80:31:59"
+          }
+        }
+
+        ny_to_panama = Roadtrip.new('new york,ny', 'panama city,panama', roadtrip_data)
+
+        expect(ny_to_panama.travel_time).to eq('3d8h31m')
       end
     end
   end
